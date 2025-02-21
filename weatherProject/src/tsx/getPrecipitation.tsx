1d41405cb4e3b0b3d71ac2f4035f529e9db86195
NotNullEx <111674240+NotNullEx@users.noreply.github.com>
2025-02-21 09:38:04 +0900

    api
    api 데이터 받아옴

---
+++ weatherProject/src/tsx/getPrecipitation.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect,useState } from "react";
+import axios from "axios";
+
+interface WeatherInfo {
+    tm_max: string;
+    cnt7: string | null;
+    cnt8: string | null;
+    cnt5: string;
+    cnt6: string | null;
+    cnt3: string;
+    stn_ko: string; // 지역명
+    cnt4: string | null;
+    cnt1: string;
+    max_rn_day: string;
+    cnt2: string;
+    tm_rn_day: string;
+    ev_s: string | null;
+    wd_max: string;
+    rn_day_cnt2: string;
+    rn_day_cnt1: string;
+    rn_day_cnt4: string;
+    rn_day_cnt3: string;
+    ws_max: string;
+    rn_day: string;
+    rn: string;
+    ws: string;
+    stn_id: number; // 지역번호
+    cnt9: string | null;
+}
+
+interface ApiResponse {
+    response: {
+        body: {
+            items: {
+                item: { month: { info: WeatherInfo[] } }[];
+            };
+        };
+    };
+}
+const CallApi: React.FC = () => {
+    const [weatherData, setWeatherData] = useState<WeatherInfo[] | null>(null);
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            const apiUrl =
+            "/api/typ02/openApi/SfcMtlyInfoService/getMmSumry2?pageNo=1&numOfRows=10&dataType=json&year=2024&month=12&authKey=SbnYSQwuQ-G52EkMLhPhlQ";
+
+            try {
+                const response = await fetch(apiUrl);
+                if (!response.ok) {
+                throw new Error(`HTTP 오류: ${response.status}`);
+                }
+
+                const data: ApiResponse = await response.json();
+                console.log("API 응답 데이터:", data.response.body.items.item[0].month.info[0]);
+
+                // API 데이터에서 필요한 정보 추출
+                const weatherInfo = data.response.body.items.item[0].month.info[0];
+                console.log(weatherInfo);
+                // setWeatherData(weatherInfo);
+                // axios
+                // .get(apiUrl)
+                // .then((response) => {
+                //     console.log("API 응답:", response.data);
+
+                // })
+                // .catch((error) => {
+                //     console.error("API 호출 중 오류 발생:", error);
+                // });
+            } catch (error) {
+                console.error("API 호출 중 오류 발생:", error);
+            }
+        }
+       
+        fetchWeatherData();
+        
+    }, []);
+
+    return <div>API 호출 중...</div>;
+};
+
+export default CallApi;
