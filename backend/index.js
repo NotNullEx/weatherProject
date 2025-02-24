7e1a709768b12b91533b81ea5d2b0cedd43ca30c
WhiteCloud123 <lghlgh258000@naver.com>
2025-02-24 18:39:06 +0900

    sql
    sql

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import mysql from 'mysql2';
+
+// MySQL 연결 설정
+const connection = mysql.createConnection({
+  host: 'mysql-db',
+  user: 'weather',
+  password: '1q2w3e',
+  database: 'weatherDB',
+});
+
+// 연결 시도
+connection.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결 실패:', err);
+    return;
+  }
+  console.log('MySQL 연결 성공!');
+
+  // 데이터 삽입 예제
+  const query = `
+    INSERT INTO month_temperature_weather
+    (id, city, year, month, taavg, tamax, tamin, avghm)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  const values = [null, 'seoul', 2024, 12, 1.1, 2.2, 3.3, 60];
+
+  connection.query(query, values, (err, result) => {
+    if (err) {
+      console.error('데이터 삽입 실패:', err);
+    } else {
+      console.log('데이터 삽입 성공:', result);
+    }
+    connection.end();
+  });
+});
--- weatherProject/init.sql
+++ weatherProject/init.sql
@@ -2,16 +2,37 @@ CREATE DATABASE IF NOT EXISTS weatherDB;
 
 USE weatherDB;
 
-CREATE TABLE IF NOT EXISTS month_weather (
+CREATE TABLE IF NOT EXISTS month_temperature_weather (
   id INT(1) AUTO_INCREMENT PRIMARY KEY,
   city varchar(50) not null,
   year smallint not null,
   month smallint not null,
-  taavg float not null,
-  tamax float not null,
-  tamin float not null,
-  avghm smallint not null,
-  rnDay float not null,
-  maxRnDay float not null,
-  tmRnDay tinyint not null
+  taavg float not null,   -- 기온 평균
+  tamax float not null,   -- 기온 최고
+  tamin float not null,   -- 기온 최저
+  avghm smallint not null -- 평균 상대 습도
+);
+
+create table if not exists month_precipitation_weather(
+  id int(1) auto_increment primary key,
+  city varchar(50) not null,
+  year smallint not null,
+  rnDay float not null,    -- 강수량 총량
+  maxRnDay float not null, -- 1일 최다 강수량
+  tmRnDay tinyint not null -- 1일 최다 강수량이 나타난 날
+);
+
+create table if not exists year_temper_precipi_weather(
+  id int(1) auto_increment primary key,
+  city varchar(50) not null,
+  year smallint not null,
+  Va_lst_11 float not null,   -- 강수량 총량
+  Va_lst_13 float not null,   -- 1일 최다 강수량
+  Va_lst_14 tinyint not null, -- 1일 최다 강수량이 나타난 날
+  Va_lst_03 float not null,   -- 평균 기온
+  Va_lst_05 float not null,   -- 최고 평균 기온
+  Va_lst_06 float not null,   -- 최고 기온
+  Va_lst_07 tinyint not null, -- 최고 기온이 나타난 날
+  Va_lst_09 float not null,   -- 최저 기온
+  Va_lst_10 tinyint not null  -- 최저 기온이 나타난 날
 );
